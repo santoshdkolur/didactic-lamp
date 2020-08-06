9d705ce4640c721746c5d66825755b330c73b223
Santosh Kolur <48786464+santoshdkolur@users.noreply.github.com>
2020-08-06 22:15:58 +0530

    Create consecutiveSum.py

---
+++ consecutiveSum.py
@@ -0,0 +1,20 @@
+'''How many ways can a sum of consecutive numbers be expressed to represent a number N
+    For ex: Say N is 10, then we can get n by '1+2+3+4' and no other combination. Hence the output should be 1'''
+
+
+
+from math import ceil
+n=int(input())
+count=0
+for i in range(1,ceil(n/2)+1):
+    s=0
+    for j in range(i,ceil(n/2)+1):
+        s=s+j 
+        if s==n:
+            for x in range(i,j+1):
+                print(x,end=' ')
+                
+            print('')
+            count+=1 
+            break
+print(count)
