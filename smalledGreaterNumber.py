50e7a29044ae065dc5f468e53b07f90f57e47077
Santosh Kolur <48786464+santoshdkolur@users.noreply.github.com>
2020-08-07 12:17:40 +0530

    Create smalledGreaterNumber.py

---
+++ smalledGreaterNumber.py
@@ -0,0 +1,31 @@
+'''
+Compute the nearest larger number by interchanging its digits updated.
+Given 2 numbers a and b find the smallest number greater than b by interchanging the digits of a and if not possible print -1.
+
+Sample Input:
+
+    459 500
+
+Sample Output:
+    549
+    
+'''
+
+n=input().split(' ')
+from itertools import permutations
+
+p=permutations(n[0])
+a=[]
+for i in list(p):
+    a.append(int(''.join(i)))
+
+a.sort()
+flag=0
+for ele in a:
+    if ele>int(n[1]):
+        print(ele)
+        flag=1 
+        break
+
+if(flag==0):
+    print('-1')
