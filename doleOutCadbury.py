dbc830714efd549fd773c11a9d7dba314c9823a5
Santosh Kolur <48786464+santoshdkolur@users.noreply.github.com>
2020-08-07 21:51:55 +0530

    Create doleOutCadbury.py

---
+++ doleOutCadbury.py
@@ -0,0 +1,77 @@
+'''
+Dole Out Cadbury
+Problem Description
+You are a teacher in reputed school. During Celebration Day you were assigned a task
+to distribute Cadbury such that maximum children get the chocolate. You have a box
+full of Cadbury with different width and height. You can only distribute largest square
+shape Cadbury. So if you have a Cadbury of length 10 and width 5, then you need to
+break Cadbury in 5X5 square and distribute to first child and then remaining 5X5 to
+next in queue
+Constraints
+0<P<Q<1501
+0<R<S<1501
+Input Format
+First line contains an integer P that denotes minimum length of Cadbury in the box
+Second line contains an integer Q that denotes maximum length of Cadbury in the box
+Third line contains an integer R that denotes minimum width of Cadbury in the box
+Fourth line contains an integer S that denotes maximum width of Cadbury in the box
+Output
+Print total number of children who will get chocolate.
+Test Case
+Explanation
+Example 1
+Input
+5
+7
+3
+4
+Output
+24
+
+Explanation
+Length is in between 5 to 7 and width is in between 3 to 4.
+So we have 5X3,5X4,6X3,6X4,7X3,7X4 type of Cadbury in the box.
+If we take 5X3 :
+First, we can give 3X3 square Cadbury to 1st child .Then we are left with 3X2. Now
+largest square is 2X2 which will be given to next child. Next, we are left with two 1X1
+part of Cadbury which will be given to another child.
+And so on
+
+'''
+
+
+l_min=int(input())
+l_max=int(input())
+w_min=int(input())
+w_max=int(input())
+
+combinations =[]
+
+for i in range(l_min,l_max+1):
+    for j in range(w_min,w_max+1):
+        combinations.append([i,j])
+        
+count=0
+
+def calc(ele):
+    global count
+    if(ele[0]==1 and ele[1]==1):
+        count+=1
+        return
+    
+    if(ele[0]>ele[1]):
+        ele[0]=ele[0]-ele[1]
+        count=count+1
+    elif ele[1]>ele[0]:
+        ele[1]=ele[1]-ele[0]
+        count=count+1
+    else:
+        count=count+1 
+        return
+    
+    calc(ele)
+    
+for ele in combinations:
+    calc(ele)
+
+print(count)
