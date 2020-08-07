40496d842e7dea4e813ddadf58b198f466597470
Santosh Kolur <48786464+santoshdkolur@users.noreply.github.com>
2020-08-07 23:34:07 +0530

    Create formTriangles.py

---
+++ formTriangles.py
@@ -0,0 +1,37 @@
+'''
+From the given inputs find how many number of triangles can be formed. 
+Input : N number of slopes of n different lines. 
+
+Note : Slope--> angle formed by the line wrt x axis
+
+Example 1:
+
+Input 
+20 -20 0 50 50
+
+Output
+7
+
+
+Example 2:
+
+Input
+50 -50 50 -50 50
+
+Output
+0
+
+
+'''
+
+
+from itertools import combinations
+n=int(input())
+slopes=[int(ele) for ele in input().split(' ')]
+count=0
+for ele in combinations(slopes,3):
+    if(len(ele)>len(set(ele))):
+        continue
+    else:
+        count=count+1
+print(count)
