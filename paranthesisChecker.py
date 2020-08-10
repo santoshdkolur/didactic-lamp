310e2c39ae7e674df0bd4923079b19afaf450dd7
Santosh Kolur <48786464+santoshdkolur@users.noreply.github.com>
2020-08-10 23:37:02 +0530

    Create paranthesisChecker.py

---
+++ paranthesisChecker.py
@@ -0,0 +1,68 @@
+'''
+Given two sorted arrays arr1[] and arr2[] in non-decreasing order with size n and m. The task is to merge the two sorted arrays into one sorted array (in non-decreasing order).
+
+Note: Expected time complexity is O((n+m) log(n+m)). DO NOT use extra space.  We need to modify existing arrays as following.
+
+Input: arr1[] = {10};
+       arr2[] = {2, 3};
+Output: arr1[] = {2}
+        arr2[] = {3, 10}  
+
+Input: arr1[] = {1, 5, 9, 10, 15, 20};
+       arr2[] = {2, 3, 8, 13};
+Output: arr1[] = {1, 2, 3, 5, 8, 9}
+        arr2[] = {10, 13, 15, 20} 
+Input:
+First line contains an integer T, denoting the number of test cases. First line of each test case contains two space separated integers X and Y, denoting the size of the two sorted arrays. Second line of each test case c…
+[10:51 PM, 8/10/2020] Nisarg Sjpuc: Given an expression string exp. Examine whether the pairs and the orders of “{“,”}”,”(“,”)”,”[“,”]” are correct in exp.
+For example, the program should print 'balanced' for exp = “[()]{}{[()()]()}” and 'not balanced' for exp = “[(])”
+
+Input:
+The first line of input contains an integer T denoting the number of test cases.  Each test case consists of a string of expression, in a separate line.
+
+Output:
+Print 'balanced' without quotes if the pair of parenthesis is balanced else print 'not balanced' in a separate line.
+
+Constraints:
+1 ≤ T ≤ 100
+1 ≤ |s| ≤ 105
+
+Example:
+Input:
+3
+{([])}
+()
+([]
+
+Output:
+balanced
+balanced
+not balanced
+
+'''
+
+
+
+
+n=int(input())
+for _ in range(n):
+    q=list(input())
+    stk=[]
+    op=['{','[','(']
+    cl=['}',']',')']
+    for ele in q:
+        if ele in op:
+            stk.append(ele)
+        elif ele in cl:
+            try:
+                if ele==cl[op.index(stk[len(stk)-1])]:
+                    stk.pop()
+                else:
+                    break
+            except:
+                stk.append(ele)
+                break
+    if(len(stk)!=0):
+        print("Not Balanced")
+    else:
+        print("Balanced")
