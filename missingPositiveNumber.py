ba91b01c5ea14d81e1234781c20a6cfb75932bd9
Santosh Kolur <48786464+santoshdkolur@users.noreply.github.com>
2020-08-28 09:33:45 +0530

    Create missingPositiveNumber.py

---
+++ missingPositiveNumber.py
@@ -0,0 +1,16 @@
+'''
+Given an array of integers, find the first missing positive integer in linear time and constant space. 
+In other words, find the lowest positive integer that does not exist in the array. 
+The array can contain duplicates and negative numbers as well.
+
+For example, the input [3, 4, -1, 1] should give 2. The input [1, 2, 0] should give 3.
+'''
+
+n=[int(ele) for ele in input().split(' ')]
+l=len(n)
+n=[ele for ele in n if ele>0]
+m=min(n)
+for i in range(m,m+l):
+    if i not in n:
+        print(i)
+        break
