a5fa7d42b3beb300e12fc0a2526a9b7b1d0ee46f
Santosh Kolur <48786464+santoshdkolur@users.noreply.github.com>
2020-09-09 23:16:37 +0530

    Update anagramString.java

--- anagramString.java
+++ anagramString.java
@@ -1,10 +1,14 @@
 /*
- * Anagrams are words or phrases you spell by rearranging the letters of another word or phrase. For instance, fans of the Harry Potter series know that Lord Voldemort’s full name is actually an anagram of his birth name,
- * 	and some people even play games challenging one another to make anagrams still relevant to the original term.
- *  For example, "schoolmaster" can be turned into "the classroom", "punishments" becomes "nine thumps", and "debit card" turns into "bad credit".
- *	The only rule is that all the letters from the original word or phrase must be used when they’re reordered to say something entirely different.
- * 	Author:Nisarg Devdhar
+ Anagrams are words or phrases you spell by rearranging the letters of another word or phrase. For instance, fans of the Harry Potter series know that Lord Voldemort’s full name is actually an anagram of his birth name, 
+ and some people even play games challenging one another to make anagrams still relevant to the original term.
+ For example, "schoolmaster" can be turned into "the classroom", "punishments" becomes "nine thumps", and "debit card" turns into "bad credit".
+ The only rule is that all the letters from the original word or phrase must be used when they’re reordered to say something entirely different.
  */
+
+
+//Author:Nisarg Devdhar
+
+
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Scanner;
