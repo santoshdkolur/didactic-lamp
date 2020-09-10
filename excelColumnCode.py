08d9c3ba7b03cdbf43ae24b49898a4a980a44895
Santosh Kolur <48786464+santoshdkolur@users.noreply.github.com>
2020-09-10 08:56:55 +0530

    Update excelColumnCode.py

--- excelColumnCode.py
+++ excelColumnCode.py
@@ -11,23 +11,24 @@ For example:
     AA -> 27
     AB -> 28 
     ...
+    
 Example 1:
 
-Input: "A"
-Output: 1
-Example 2:
+Input: "A"  
+Output: 1  
+Example 2:  
 
-Input: "AB"
-Output: 28
-Example 3:
+Input: "AB"  
+Output: 28  
+Example 3:  
 
-Input: "ZY"
-Output: 701
+Input: "ZY"  
+Output: 701  
 
 '''
 
 
-#AUTHOR : SNATOSH D KOLUR
+#AUTHOR : SANTOSH D KOLUR
 
 
 def columnCode(n):
