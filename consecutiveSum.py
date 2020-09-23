fd42f0134b0776c81a55c1ab5678b605bc592eb1
Santosh Kolur <48786464+santoshdkolur@users.noreply.github.com>
2020-09-23 22:43:11 +0530

    Update consecutiveSum.py

--- consecutiveSum.py
+++ consecutiveSum.py
@@ -1,5 +1,10 @@
-'''How many ways can a sum of consecutive numbers be expressed to represent a number N
-    For ex: Say N is 10, then we can get n by '1+2+3+4' and no other combination. Hence the output should be 1'''
+#MEDIUM
+'''
+How many ways can a sum of consecutive numbers be expressed to represent a number N  
+For ex:  
+Say N is 10, then we can get n by '1+2+3+4' and no other combination. Hence the output should be 1
+
+'''
 
 # AUTHOR : SANTOSH D KOLUR
 
